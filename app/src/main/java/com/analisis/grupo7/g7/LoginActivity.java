b3cbc78726f978df06be2bab8d5cec875422b1d1
Carlos Rodriguez <carlosrod.furlan@gmail.com>
2015-09-22 20:16:43 -0600

    Satisfaccion de pruebas para historia 'Login en aplicacion movil'

--- app/src/main/java/com/analisis/grupo7/g7/LoginActivity.java
+++ app/src/main/java/com/analisis/grupo7/g7/LoginActivity.java
@@ -1,28 +1,50 @@
 package com.analisis.grupo7.g7;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class LoginActivity extends ActionBarActivity {
+
+    private TextView usernameT;
+    private TextView passwT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        usernameT = (TextView)findViewById(R.id.username);
+        passwT = (TextView)findViewById(R.id.password);
+
         Button loginB = (Button) findViewById(R.id.login_button);
         loginB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),EventSelect.class);
-                startActivity(intent);
+                String usuario = usernameT.getText().toString();
+                String pw = passwT.getText().toString();
+                if(validarVacios(usuario,pw)){
+                    Toast.makeText(getApplicationContext(),"Correcto",Toast.LENGTH_SHORT).show();
+                    new Loggearse(usuario,pw).execute();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Debe ingresar todos los datos",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -50,7 +72,50 @@ public class LoginActivity extends ActionBarActivity {
     }
 
     public boolean validarVacios(String user, String pass){
-        return true;
+        return !(user.equals("")||pass.equals(""));
+    }
+
+    private class Loggearse extends AsyncTask<String,Void,String>{
+
+        private String carnet;
+        private String pass;
+
+        public Loggearse(String carnet, String pass){
+            this.carnet = carnet;
+            this.pass = pass;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            URL url;
+            BufferedReader reader = null;
+
+            try{
+                url = new URL("http://carlosrodf.koding.io/android?op=pendiente");
+                URLConnection conn = url.openConnection();
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                String tmp = reader.readLine();
+                tmp = tmp.replace("&quot;","\'");
+                JSONObject obj = new JSONObject(tmp);
+                Log.d("MSG","Servidor encontrado!");
+
+            }catch (Exception e){
+                e.printStackTrace();
+                Log.d("MSG","No se encontro el sevidor");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 
 }
